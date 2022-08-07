b46ce6c96296d0225a896455b8eacb62ccfafc06
JWNicholson <john.nicholson812@gmail.com>
2022-08-07 10:23:36 -0400

    added routes for home and shop page

---
+++ src/components/routes/home/navigation/navigation.component.js
@@ -0,0 +1,23 @@
+import { Outlet, Link } from 'react-router-dom';
+import {ReactComponent as Logo} from '../../../../assets/logo.svg';
+
+import './navigation.styles.scss';
+
+function Navigation() {
+  return (
+    <>
+    <div className='navigation'>
+      <Link to={'/'} className='logo-container'>
+      <div><Logo className='logo'/></div>
+      </Link>
+      
+      <div className='nav-links-container'>
+        <Link to={'/shop'} className='nave-link'>SHOP</Link>
+      </div>
+    </div>
+    <Outlet />
+  </>
+  );
+}
+
+export default Navigation;
